530af6ff3ae710c2ad9acf0bf117433fb4a47c85
fleece30 <abhiyad25@gmail.com>
2019-09-15 17:02:37 +0530

    server-status

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var path = require('path');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.uri, {useNewUrlParser: true, useUnifiedTopology: true}, function(err){
+    if(err){
+        console.log('Nope ',err);
+    }
+    else{
+        console.log(config.secret);
+        console.log('Yep'+config.db);
+    }
+});
+
+app.use(express.static(__dirname + '/client/dist'));
 
 app.get('*', function (req, res) {
-    res.send('hells world')
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'))
   })
   
   app.listen(8080, function(){
