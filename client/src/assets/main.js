3e14e283e8c2b0baf839643c025e9dbf1aae8e9e
fleece30 <abhiyad25@gmail.com>
2019-10-28 00:43:35 +0530

    commit

--- client/src/assets/main.js
+++ client/src/assets/main.js
@@ -18,9 +18,8 @@ var app=angular.module("form",[])
     var request = $http(config);
     request.then(function(response){
       for(var i=0;i<response.data['data'].length;i++){
-        // $("#ailments").append("<li>"+response.data['data'][i]['ailment']+" | "+response.data['data'][i]['severity']+"</li>");
         $("#ailments").append(
-          "<div class='card' style='color: black; width: 70%;'><div class='card-header'><h3 class='card-title'><strong>Diagnosis: </strong>"+response.data['data'][i]['ailment']+"</h3></div><div class='card-block' style='padding: 20px;'><strong>Severity: </strong>"+response.data['data'][i]['severity']+"</div><div class='card-footer'><strong>Symptoms: </strong>"+response.data['data'][i]['symptoms']+"</div></div><br><br>"
+          "<div class='card' style='color: black; width: 70%;'><div class='card-header'><h3 class='card-title'><strong>Diagnosis: </strong>"+response.data['data'][i]['ailment']+"</h3></div><div class='card-block' style='padding: 20px;'><strong>Severity: </strong>"+response.data['data'][i]['severity']+"</div><div class='card-footer'><strong>Symptoms: </strong>"+response.data['data'][i]['symptoms']+"<br><strong>Medicines: </strong>"+response.data['data'][i]['medicine']+"</div></div><br><br>"
         )
       }
     },
