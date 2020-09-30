a3ae420f48df936db397d7e47235e3ba674cba72
fleece30 <abhiyad25@gmail.com>
2020-09-30 21:27:16 +0530

    hack issues

--- routes/blogs.js
+++ routes/blogs.js
@@ -1,118 +1,69 @@
-const Blog = require('../models/blog');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const config = require('../config/database');
+const Blog = require("../models/blog");
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("../config/database");
 
+module.exports = function (router) {
+  router.post("/newBlog", (req, res) => {
+    if (!req.body.title) {
+      res.json({ success: false, message: "Blog title is required." });
+    } else {
+      if (!req.body.body) {
+        res.json({ success: false, message: "Blog body is required." });
+      } else {
+        const blog = new Blog({
+          title: req.body.title,
+          body: req.body.body,
+          createdBy: req.body.createdBy,
+        });
+        blog.save((err) => {
+          if (err) {
+            res.json({ success: false, message: err });
+          } else {
+            res.json({ success: true, message: "Blog Saved" });
+          }
+        });
+      }
+    }
+  });
 
-module.exports=function(router){
-    router.post('/newBlog',(req,res) => {
-        if(!req.body.title){
-            res.json({success: false, message: 'Blog title is required.'});
+  router.get("/allBlogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else {
+        if (!blogs) {
+          res.json({ success: false, message: "No blogs" });
+        } else {
+          res.json({ success: true, blogs: blogs });
         }
-        else{
-            if(!req.body.body){
-                res.json({success: false, message: 'Blog body is required.'});
+      }
+    }).sort({ _id: -1 });
+  });
+
+  //Add code here
+
+  router.put("/updateBlog", (req, res) => {
+    Blog.findOne({ _id: req.body._id }, (err, blogs) => {
+      if (err) {
+        res.json({ success: false, message: err });
+      } else {
+        if (!blogs) {
+          res.json({ success: false, message: "No blogs" });
+        } else {
+          blogs.title = req.body.title;
+          blogs.body = req.body.body;
+          blogs.save((err) => {
+            if (err) {
+              res.json({ success: false, message: err });
+            } else {
+              res.json({ success: true, message: "Blog Updated" });
             }
-            else{
-                const blog = new Blog({
-                    title: req.body.title,
-                    body: req.body.body,
-                    createdBy: req.body.createdBy
-                });
-                blog.save((err) => {
-                    if(err){
-                        res.json({success: false, message: err});
-                    }
-                    else{
-                        res.json({success: true, message: 'Blog Saved'});
-                    }
-                })
-            }
+          });
         }
+      }
     });
+  });
 
-    router.get('/allBlogs',(req,res) => {
-        Blog.find({}, (err,blogs) => {
-             if(err){
-                 res.json({success: false, message: err});
-             }
-             else{
-                 if(!blogs){
-                    res.json({success: false, message: "No blogs"});
-                 }
-                 else{
-                     res.json({success: true, blogs: blogs});
-                 }
-             }
-        }).sort({'_id':-1});
-    });
-
-    router.get('/singleBlog/:id',(req,res) => {
-        Blog.findOne({ _id: req.params.id}, (err,blogs) =>{
-            if(err){
-                res.json({success: false, message: err});
-            }
-            else{
-                if(!blogs){
-                   res.json({success: false, message: "No blogs"});
-                }
-                else{
-                    res.json({success: true, blogs: blogs});
-                }
-            }
-        });
-    });
-
-    router.put('/updateBlog', (req, res) => {
-        Blog.findOne({ _id: req.body._id }, (err,blogs) => {
-            if(err){
-                res.json({success: false, message: err});
-            }
-            else{
-                if(!blogs){
-                   res.json({success: false, message: "No blogs"});
-                }
-                else{
-                    blogs.title = req.body.title;
-                    blogs.body = req.body.body;
-                    blogs.save((err) => {
-                        if(err){
-                            res.json({success: false, message: err});
-                        }
-                        else{
-                            res.json({success: true, message: 'Blog Updated'});
-                        }
-                    });
-                }
-            }
-        });
-    });
-
-    router.delete('/deleteBlog/:id', (req,res) => {
-        Blog.findOne({_id: req.params.id}, (err,blogs) => {
-            if(err){
-                console.log(err);
-                res.json({success: false, message: err});
-            }
-            else{
-                if(!blogs){
-                   res.json({success: false, message: "No blogs"});
-                }
-                else{
-                    blogs.remove((err) => {
-                        if(err){
-                            res.json({success: false, message: err});
-                        }
-                        else{
-                            res.json({success: true, message: 'Blog Removed'});
-                        }
-                    });
-                }
-            }
-        });
-    });
-
-    
-
-    return router;
+  return router;
 };
